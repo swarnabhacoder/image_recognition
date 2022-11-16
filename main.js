f8980f486ce2fa9ef5587ab0942245b4ab4ebeb5
swarnabhacoder <103578241+swarnabhacoder@users.noreply.github.com>
2022-11-16 17:16:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+//https://teachablemachine.withgoogle.com/models/vQmFCot8g/
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+})
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+    })
+}
+
+console.log('ml5 version',ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/vQmFCot8g/model.json',modelLoaded);
+function modelLoaded(){
+    console.log('modelLoaded');
+}
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+
+}
+
+function gotResult(error,results){
+if(error){
+    console.error(error);
+
+}else{
+    console.log(results);
+    document.getElementById("object_name").innerHTML=results[0].label;
+    document.getElementById("object_accuracy").innerHTML=results[0].confidence.toFixed(3)*100+"%";
+}
+}
